b5db7590c42da4d94a3414797ddea0f98549858a
parduseidolon <38515818+PardusEidolon@users.noreply.github.com>
2022-08-11 16:07:21 +1200

    stock quotes haddock documentation for params and quotesData.

--- misc/stockquotes/src/Params.hs
+++ misc/stockquotes/src/Params.hs
@@ -1,9 +1,20 @@
--- module Params (Params (..), cmdLineParser) where
 module Params where
 
 import Options.Applicative
 import Data.Text (Text, strip)
 
+-- * Documentation
+-- | A Definition list for our type below
+-- 
+--      [@fname@]: Our file path the type here is an alias for string
+-- 
+--      [@company@]: company name to show on the report
+-- 
+--      [@chart@]: Wheather or not we generate our candle stick and bar charts
+-- 
+--      [@htmlfile@]: Our file path to generate an html file should we choose to do so
+-- 
+--      [@silent@]: If True we don't generate the stat report. 
 data Params = Params { fname :: FilePath
                      , company :: Maybe Text
                      , chart :: Bool
@@ -11,33 +22,36 @@ data Params = Params { fname :: FilePath
                      , silent :: Bool
                      } deriving Show
 
--- depending on how we want out CLI to work we fist need to describe our options for the parser
-
-
+-- ** Functions
+-- | Our constructor for the datatype @__Parser__@ we use this function to apply the functions to construct values for our @__Params__@ datatype.
+-- 
+-- We have the following functions which define what value gets applied to the constructor of the type:
+-- 
+--      [@strArgument@]: A builder for out string argument we use this for our filpath.
+-- 
+--      [@optional@]: Provides structure for the @company@ field which is also using Text
+-- 
+--      [@switch@]: A builder for a @Boolean@ field for @Parser@
+-- 
+-- Beccause where using applicative here, were partially applying to the data contructor Params using fmap to preserve the structure...
+-- to eventually then give us an output of Parser Params
+-- 
+-- We are preserving Parser by lifting it, taking out type Params and partially applying until we fill out all the fields.
+-- after weve fully applied our type we then lift Parser back down and get Parser Params
 mkParams :: Parser Params
 mkParams = 
     Params <$> strArgument
-                    (metavar "FILE" <> help "CSV file name") -- a builder for a string argument
-                    -- applies ^ to our fname field
+                    (metavar "FILE" <> help "CSV file name")
                 <*> optional (strip <$> strOption 
-                        (long "name" <> short 'n' <> help "Company name")) -- optional is a useful modelling tool to allow things to fail
-                        -- applies ^ to our company field
+                        (long "name" <> short 'n' <> help "Company name"))
                 <*> switch
                         (long "Chart" <> short 'c' <> help "Generate chart")
-                        -- applies ^ to our chart field
                 <*> optional (strOption $
                         long "html" <> metavar "FILE" <> help "Generate Html report")
-                        -- applies to our htmlFile field
                 <*> switch
                         (long "silent" <> short 's' <> help "Don't print statistics")
-                        -- applies to our silent filed thus ending out application and leaving us with the type Parser Params
 
--- Beccause where using applicative here, were partially applying to the data contructor Params using fmap to preserve the structure...
--- to eventually then give us an output of Parser Params
-
--- We are preserving Parser by lifting it, taking out type Params and partially applying until we fill out all the fields.
--- after weve fully applied our type we then lift Parser back down and get Parser Params
-
+-- | Takes our Params type and executes. It also creates information for our Params by taking The helper combinator which takes any parser, and adds a help option to it.
 cmdLineParser :: IO Params
 cmdLineParser = execParser opts
     where
@@ -46,7 +60,3 @@ cmdLineParser = execParser opts
                                          <> footer "This program was created by jack@yumi.ai" 
                                          <> progDesc "Stock quotes data proccessing for generating html reports and charts from CSV files."
                                           )
--- Takes our Params type and executes, with a little bit of help form our friends 'info' which are just details and header smooshed at the end of the day to produce InfoMod Params
--- the library takes care of most of it so you dont have to.
-
--- (helper <*> mkParams) :: Parser (Params -> Params) -> Parser Params -> ParserInfo Params 
--- misc/stockquotes/src/QuotesData.hs
+++ misc/stockquotes/src/QuotesData.hs
@@ -10,6 +10,15 @@ import Data.Csv (FromNamedRecord, FromField (..))
 
 import Data.Maybe
 
+-- * Quote Data Documentation
+-- | A data type to interperate the CSV fields.
+-- The following fields:
+-- 
+--  [@Day@]: Were taking the type @Day@ from the 'Time' package in the format YYYY-MM-DD
+--  [@Volume@]: Because volume is a whole number we will have to generalise this later when generating the reports
+--  [@..@] As for the rest of the remainning fields, we are only dealing with floating point numbers.
+-- 
+-- Because weve imported cassava @QuotesData@ needs to have a FromNamedRecord instance to convert the CSV records from user defined data types.
 data QuoteData = QuoteData {
     day :: Day ,
     volume :: Int ,
@@ -19,9 +28,22 @@ data QuoteData = QuoteData {
     low :: Double 
     } deriving (Generic, FromNamedRecord, Show)
 
+-- | Defines how to convert the day field into a value we care about. Because the type doesnt have an instance we need to define on ourselves.
+-- 
+--  we can flip the dates around if we want a differnt format:
+-- 
+-- >>> parseField = parseTimeM True defaultTimeLocale "%m-%d-%Y" . unpack
+
 instance FromField Day where
     parseField = parseTimeM True defaultTimeLocale "%Y-%m-%d" . unpack --regular normal date format YYYY-MM-DD
-    -- parseField = parseTimeM True defaultTimeLocale "%m-%d-%Y" . unpack -- American flipped date format MM-DD-YYYY
+
+-- | Unwrap data type for out number fields, Take note that when parsing the @Volume@ field it's converted to a type of a double because of the integral converstion
+-- we get a type of:
+-- 
+-- >>> field2fun Volume = fromIntegral . volume :: Double
+-- 
+-- fromIntegral is taking our Integer and generalising it to be an ad hoc polymophic type constrained by the @Num@ class and because of our defined output for the function
+-- The compiler is able to interpret and spit out a @Double@
 
 data QField = Open | Close | High | Low | Volume deriving (Eq,Show,Enum,Ord,Bounded)
 
@@ -32,7 +54,7 @@ field2fun High = high
 field2fun Low = low
 field2fun Volume = fromIntegral . volume
 
--- Example Values
+-- | Example Values
 
 exampleQuote :: QuoteData
 exampleQuote = QuoteData (fromGregorian 2022 07 25) (120234) (12.83) (13.48) (13.49) (12.82)
